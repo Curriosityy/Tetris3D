99899b4a3645adb9f3b22bf449d57cfee45e1a98
Curriosityy <patryk9595@gmail.com>
2019-11-08 17:28:48 +0100

    +created dummy spawner class and tetrimino class

---
+++ Assets/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    PawnPooler pool;
+    List<Tetrimino> tetriminos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pool = PawnPooler.Instance;
+        tetriminos = new List<Tetrimino>();
+        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(2, 0, 0), new Vector3(3, 0, 0) }));
+        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(2, 0, 0), new Vector3(2, 1, 0) }));
+        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 1, 0), new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(2, 0, 0) }));
+        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(1, 1, 0), new Vector3(2, 1, 0) }));
+        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 1, 0), new Vector3(1, 1, 0), new Vector3(1, 0, 0), new Vector3(2, 0, 0) }));
+        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0) }));
+        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(2, 0, 0), new Vector3(1, 1, 0) }));
+        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(1, 1, 0), new Vector3(1, 1, 1) }));
+        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 1, 0), new Vector3(1, 1, 0), new Vector3(1, 0, 0), new Vector3(1, 0, 1) }));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Tetrimino.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Tetrimino
+{
+    Vector3[] pos;
+    public Vector3[] Pos
+    {
+        get
+        {
+            return pos;
+        }
+    }
+    public Tetrimino(Vector3[] positions)
+    {
+        pos = positions;
+    }
+    public Tetrimino(Vector3 pos1, Vector3 pos2, Vector3 pos3, Vector3 pos4)
+    {
+        pos = new Vector3[4];
+        pos[0] = pos1;
+        pos[1] = pos2;
+        pos[2] = pos3;
+        pos[3] = pos4;
+    }
+
+
+}
+
