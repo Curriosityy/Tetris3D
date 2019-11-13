2bda310dc9a3b5b1239c4877597abac01d99c354
Curriosityy <patryk9595@gmail.com>
2019-11-13 10:28:50 +0100

    +added empty socket class +added empty boardcreator script

--- Assets/BoardCreator.cs
+++ Assets/BoardCreator.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : Singleton<GameManager>
+
+
+public class BoardCreator : MonoBehaviour
 {
+    int x=8, y=8;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
--- Assets/Scripts/PawnControler.cs
+++ Assets/Scripts/PawnControler.cs
@@ -5,30 +5,49 @@ using UnityEngine;
 public class PawnControler : MonoBehaviour
 {
     TetrionInitializer pawn;
+
+    public TetrionInitializer Pawn
+    {
+        get
+        {
+            return pawn;
+        }
+
+        set
+        {
+            pawn = value;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        pawn = FindObjectOfType<Spawner>().SpawnPawn();
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.DownArrow))
+        if(pawn!=null)
         {
-            pawn.transform.Translate(Vector3.back);
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            pawn.transform.Translate(Vector3.forward);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            pawn.transform.Translate(Vector3.left);
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            pawn.transform.Translate(Vector3.right);
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                pawn.transform.Translate(Vector3.back);
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                pawn.transform.Translate(Vector3.forward);
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                pawn.transform.Translate(Vector3.left);
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                pawn.transform.Translate(Vector3.right);
+            }
         }
     }
-    
 }
