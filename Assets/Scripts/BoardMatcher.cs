5b06eb7f371a0b49cb917220c5d1f5583a8393a4
Curriosityy <patryk9595@gmail.com>
2019-11-13 13:39:01 +0100

    +boardmatcher check if layer is full. If it is then deactivate every tetris part in layer.

---
+++ Assets/Scripts/BoardMatcher.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardMatcher : MonoBehaviour
+{
+    List<Socket[,]> socketLayers;
+    void Start()
+    {
+        socketLayers = GetComponent<BoardCreator>().SocketLayers;
+    }
+    private bool CheckIfLayerIsFull(Socket[,] layer)
+    {
+        foreach(var socket in layer)
+        {
+            if(!socket.CheckTetrisSocket())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    private void DestroyLayers()
+    {
+        foreach (var layer in socketLayers)
+        {
+            if(CheckIfLayerIsFull(layer))
+            {
+                DeactivateLayer(layer);
+            }
+        }
+    }
+    private void DeactivateLayer(Socket[,] layer)
+    {
+        foreach(var socket in layer)
+        {
+            socket.TetrisPart.SetActive(false);
+            socket.TetrisPart = null;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
