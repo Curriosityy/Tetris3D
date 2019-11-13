7e79c28146d46f4cffeeb76df7af31777c30e63e
Curriosityy <patryk9595@gmail.com>
2019-11-13 13:34:31 +0100

    +added socket with contain socket pos and tetris part. Also we can check if socket contain tetris part.

--- Assets/Scripts/Socket.cs
+++ Assets/Scripts/Socket.cs
@@ -1,4 +1,33 @@
-﻿class Socket
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Socket
 {
+    Vector3 socketPos;
+    GameObject tetrisPart;
+    public Socket(Vector3 pos)
+    {
+        SocketPos = pos;
+    }
+
+    public GameObject TetrisPart{get;}
+
+    public Vector3 SocketPos
+    {
+        get;
+    }
+
+    public bool CheckTetrisSocket()
+    {
+        RaycastHit raycast;
+        if (Physics.SphereCast(SocketPos, .4f, Vector3.forward, out raycast))
+        {
+            tetrisPart = raycast.collider.gameObject;
+            return true;
+        }
+        return false;
+        
+    }
 }
 
