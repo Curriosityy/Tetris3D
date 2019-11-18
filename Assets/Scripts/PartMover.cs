70a2ff4027f8b7ac1fd511a29f7260fcc15c79a5
Curriosityy <patryk9595@gmail.com>
2019-11-18 21:10:53 +0100

    +simple part mover to check if part can move here

---
+++ Assets/Scripts/PartMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartMover : MonoBehaviour
+{
+    Socket _currentSocket;
+
+    public Socket CurrentSocket { get => _currentSocket; set => _currentSocket = value; }
+
+    public bool IsCanFallDown()
+    {
+        return _currentSocket.Under.TetrisPart == null ? true : false;
+    }
+    public bool IsCanMoveLeft()
+    {
+        return _currentSocket.Left.TetrisPart == null ? true : false;
+    }
+    public bool IsCanMoveRight()
+    {
+        return _currentSocket.Right.TetrisPart == null ? true : false;
+    }
+    public bool IsCanMoveBackward()
+    {
+        return _currentSocket.Backward.TetrisPart == null ? true : false;
+    }
+    public bool IsCanMoveForward()
+    {
+        return _currentSocket.Forward.TetrisPart == null ? true : false;
+    }
+}
