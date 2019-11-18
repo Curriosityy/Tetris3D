56797beaf0e6edc49e8a68bd1dbe76b57532be18
Curriosityy <patryk9595@gmail.com>
2019-11-18 17:25:47 +0100

    -renamed variabled -added SettingUpInGraph that put tetris part into socket;

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,37 +5,47 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField]
-    PawnPooler pool;
-    List<Tetrimino> tetriminos;
-    Socket socket;
+    PawnPooler _pool;
+    List<Tetrimino> _tetriminos;
+    BoardCreator _boardCreator;
     // Start is called before the first frame update
     void Start()
     {
-        tetriminos = new List<Tetrimino>();
-        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(2, 0, 0), new Vector3(3, 0, 0) }));
-        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(2, 0, 0), new Vector3(2, -1, 0) }));
-        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, -1, 0), new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(2, 0, 0) }));
-        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(1, -1, 0), new Vector3(2, -1, 0) }));
-        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, -1, 0), new Vector3(1, -1, 0), new Vector3(1, 0, 0), new Vector3(2, 0, 0) }));
-        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(0, -1, 0), new Vector3(1, -1, 0) }));
-        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(2, 0, 0), new Vector3(1, -1, 0) }));
-        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(1, -1, 0), new Vector3(1, -1, 1) }));
-        tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, -1, 0), new Vector3(1, -1, 0), new Vector3(1, 0, 0), new Vector3(1, 0, 1) }));
+        _tetriminos = new List<Tetrimino>();
+        _tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(2, 0, 0), new Vector3(3, 0, 0) }));
+        _tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(2, 0, 0), new Vector3(2, -1, 0) }));
+        _tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, -1, 0), new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(2, 0, 0) }));
+        _tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(1, -1, 0), new Vector3(2, -1, 0) }));
+        _tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, -1, 0), new Vector3(1, -1, 0), new Vector3(1, 0, 0), new Vector3(2, 0, 0) }));
+        _tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(0, -1, 0), new Vector3(1, -1, 0) }));
+        _tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(2, 0, 0), new Vector3(1, -1, 0) }));
+        _tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(1, -1, 0), new Vector3(1, -1, 1) }));
+        _tetriminos.Add(new Tetrimino(new Vector3[] { new Vector3(0, -1, 0), new Vector3(1, -1, 0), new Vector3(1, 0, 0), new Vector3(1, 0, 1) }));
+        _boardCreator = FindObjectOfType<BoardCreator>();
     }
 
     public TetrionInitializer SpawnPawn()
     {
-        pool = PawnPooler.Instance;
-        if (pool != null)
+        _pool = PawnPooler.Instance;
+        if (_pool != null)
         {
-            TetrionInitializer pawn = pool.GetFromPool();
-            Tetrimino randomTet = tetriminos[Random.Range(0, tetriminos.Count)];
+            TetrionInitializer pawn = _pool.GetFromPool();
+            Tetrimino randomTet = _tetriminos[Random.Range(0, _tetriminos.Count)];
             pawn.Initialize(randomTet.Pos);
             pawn.transform.position = transform.position;
+            SettingUpInGraph(randomTet, pawn);
             return pawn;
         }
         return null;
 
+    }
+    private void SettingUpInGraph(Tetrimino tetrimino,TetrionInitializer initializer)
+    {
+        int x = 3, y = 7, z = 3;
+        for(int i=0;i<4;i++)
+        {
+            _boardCreator.SocketLayers[y + (int)tetrimino.Pos[i].y][x + (int)tetrimino.Pos[i].x, z + (int)tetrimino.Pos[i].z].TetrisPart=initializer.Parts[i];
+        }
     }
 
 }
