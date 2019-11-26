11d18e597091baa915d1f83b3cce4d00bf9f75b8
Curriosityy <patryk9595@gmail.com>
2019-11-26 21:33:50 +0100

    -added rest of rotation

--- Assets/Scripts/PawnRotator.cs
+++ Assets/Scripts/PawnRotator.cs
@@ -28,13 +28,30 @@ public class PawnRotator : MonoBehaviour
 
     public void RotateLeft()
     {
+        RotateVertically(new Vector2(1, -1));
+    }
+    public void RotateUp()
+    {
+        RotateHorizontally(new Vector2(1, -1));
+    }
+    public void RotateRight()
+    {
+        RotateVertically(new Vector2(-1, 1));
+    }
+    public void RotateDown()
+    {
+        RotateHorizontally(new Vector2(-1, 1));
+    }
+
+    private void RotateVertically(Vector2 rotationValue)
+    {
         List<Vector3> newLocalPositions = new List<Vector3>();
         Vector3 temp;
         Vector3Int newArrayPos;
         foreach (var part in _parts)
         {
-            temp = part.RotateVertically(new Vector2(1, -1));
-            if(!part.IsRotationPosible(temp,_rootPart.CurrentSocket))
+            temp = part.RotateVertically(rotationValue);
+            if (!part.IsRotationPosible(temp, _rootPart.CurrentSocket))
             {
                 Debug.Log("cannot rotate");
                 return;
@@ -42,7 +59,7 @@ public class PawnRotator : MonoBehaviour
             newLocalPositions.Add(temp);
         }
         int i = 0;
-        foreach(var part in _parts)
+        foreach (var part in _parts)
         {
             part.GetComponent<Part>().CurrentSocket.TetrisPart = null;
             newArrayPos = part.GetNewPositionInArray(newLocalPositions[i], _rootPart.CurrentSocket);
@@ -51,16 +68,30 @@ public class PawnRotator : MonoBehaviour
             i++;
         }
     }
-    public void RotateUp()
+    private void RotateHorizontally(Vector2 rotationValue)
     {
-        
+        List<Vector3> newLocalPositions = new List<Vector3>();
+        Vector3 temp;
+        Vector3Int newArrayPos;
+        foreach (var part in _parts)
+        {
+            temp = part.RotateHorizontally(rotationValue);
+            if (!part.IsRotationPosible(temp, _rootPart.CurrentSocket))
+            {
+                Debug.Log("cannot rotate");
+                return;
+            }
+            newLocalPositions.Add(temp);
+        }
+        int i = 0;
+        foreach (var part in _parts)
+        {
+            part.GetComponent<Part>().CurrentSocket.TetrisPart = null;
+            newArrayPos = part.GetNewPositionInArray(newLocalPositions[i], _rootPart.CurrentSocket);
+            _layers[newArrayPos.y][newArrayPos.x, newArrayPos.z].TetrisPart = part.gameObject;
+            part.transform.localPosition = newLocalPositions[i];
+            i++;
+        }
     }
-    public void RotateRight()
-    {
-        
-    }
-    public void RotateDown()
-    {
-        
-    }
+
 }
