7b753f627617de689f962b8067204afb2e2dffca
Curriosityy <patryk9595@gmail.com>
2019-12-09 17:09:33 +0100

    -added class which fall every layer when we destroy layer

---
+++ Assets/LayerFaller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class LayerFaller : MonoBehaviour
+{
+    List<Socket[,]> _layers;
+
+    private void Start()
+    {
+        _layers = GetComponent<BoardCreator>().SocketLayers;
+    }
+    public void Fall(List<int> indexs)
+    {
+        foreach(var index in indexs)
+        {
+            FallOnLayer(index);
+        }
+    }
+    private void FallOnLayer(int layerIndex)
+    {
+        for(int i=layerIndex+1;i<GameManager.y;i++)
+        {
+            foreach(var socket in _layers[i])
+            {
+                if(socket.TetrisPart!=null)
+                {
+                    int a = 0;
+                    socket.TetrisPart.GetComponent<PartMover>().Fall();
+                }
+            }
+        }
+    }
+
+}
+
