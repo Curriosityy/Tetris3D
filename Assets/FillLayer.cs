21212a9e33cb02fc84c8ee99255246c34e68ad93
Curriosityy <patryk9595@gmail.com>
2019-12-09 17:08:00 +0100

    -Added little cheat, when we press F1 we will fill bottom layer

---
+++ Assets/FillLayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillLayer : MonoBehaviour
+{
+    [SerializeField]
+    GameObject part;
+    Transform testContainer;
+    // Update is called once per frame
+    private void Start()
+    {
+        testContainer = new GameObject("testContainer").transform;
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F1))
+        {
+            var layer = FindObjectOfType<BoardCreator>().SocketLayers[0];
+            foreach(var socket in layer)
+            {
+                socket.TetrisPart = Instantiate(part,socket.SocketPos,Quaternion.identity,testContainer);
+            }
+        }
+    }
+}
